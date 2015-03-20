24f5fb059f199792bc84c86d01f09dd6a6f11c74
A403163 <A403163@MC020F2C.ww930.my-it-solutions.net>
2015-03-20 14:58:59 +0100

    initial commit

--- public_html/js/vg/vgmodel.js
+++ public_html/js/vg/vgmodel.js
@@ -1,187 +1,185 @@
 var vgmodel = (function () {
-    "use strict";
-    var MAXVAL = 1000000;
-    var NCOL = vgmodelstatic.getDIM().NCOL;
-    var NROW = vgmodelstatic.getDIM().NROW;
-    var STYP = vgmodelstatic.STYP;
+   "use strict";
+   var MAXVAL = 1000000;
+   var NCOL = vgmodelstatic.getDIM().NCOL;
+   var NROW = vgmodelstatic.getDIM().NROW;
+   var STYP = vgmodelstatic.STYP;
 
-    var ord = [3, 4, 2, 5, 1, 6, 0];
-    var whoBegins;
-    var courseOfGame = [];
-    var maxLev = 4;
-    var state = vgmodelstatic.getInitialState();
+   var ord = [3, 4, 2, 5, 1, 6, 0];
+   var whoBegins;
+   var courseOfGame = [];
+   var maxLev = 4;
+   var state = vgmodelstatic.getInitialState();
 
-    function possibleMoves(state) {
-        var mvs = _.range(NCOL).filter(function (c) {
-            state.hcol[c] < NROW;
-        });
-        return mvs;
-    }
-    function init(whosTurn) {
-        state = vgmodelstatic.getInitialState();
-        state.whosTurn = whosTurn === 'player1' ? STYP.player1 : STYP.player2;
-        state.cnt = {player1: 0, player2: 0};
-        whoBegins = state.whosTurn;
-    }
+   function possibleMoves(state) {
+      var mvs = _.range(NCOL).filter(function (c) {
+         state.hcol[c] < NROW;
+      });
+      return mvs;
+   }
+   function init(whosTurn) {
+      state = vgmodelstatic.getInitialState();
+      state.whosTurn = whosTurn === 'player1' ? STYP.player1 : STYP.player2;
+      state.cnt = {player1: 0, player2: 0};
+      whoBegins = state.whosTurn;
+   }
 
-    function transitionGR(e, a) {
-        if (a === STYP.empty) {
-            return e;
-        }
-        if (a === e) {
-            return a; // or e
-        }
-        if (a !== e) {
-            return STYP.neutral;
-        }
-        if (a === STYP.neutral) {
-            return STYP.neutral;
-        }
-    }
+   function transitionGR(e, a) {
+      if (a === STYP.empty) {
+         return e;
+      }
+      if (a === e) {
+         return a; // or e
+      }
+      if (a !== e) {
+         return STYP.neutral;
+      }
+      if (a === STYP.neutral) {
+         return STYP.neutral;
+      }
+   }
 
-    function move(c, mstate) {
-        if (mstate === undefined)
-            mstate = state;
-        if (mstate.hcol[c] === NROW)
-            return 'notallowed';
-        if (mstate.isMill)
-            return 'notallowed';
-        mstate.cntMoves += 1;
-        var grs = vgmodelstatic.grs[c + NCOL * mstate.hcol[c]];
-        $.each(grs, function (n, v) {
-            var gr = mstate.grstate[v];
-            gr.occupiedBy = transitionGR(mstate.whosTurn, gr.occupiedBy);
-            if (gr.occupiedBy !== STYP.neutral)
-                gr.cnt++;
-            if (gr.cnt >= 4) {
-                mstate.isMill = true;
+   function move(c, mstate) {
+      if (mstate === undefined)
+         mstate = state;
+      if (mstate.hcol[c] === NROW)
+         return 'notallowed';
+      if (mstate.isMill)
+         return 'notallowed';
+      mstate.cntMoves += 1;
+      var grs = vgmodelstatic.grs[c + NCOL * mstate.hcol[c]];
+      forEach(grs, function (v) {
+         var gr = mstate.grstate[v];
+         gr.occupiedBy = transitionGR(mstate.whosTurn, gr.occupiedBy);
+         if (gr.occupiedBy !== STYP.neutral)
+            gr.cnt++;
+         if (gr.cnt >= 4) {
+            mstate.isMill = true;
+         }
+      });
+      mstate.hcol[c] += 1;
+      mstate.whosTurn = mstate.whosTurn === STYP.player1 ? STYP.player2 : STYP.player1;
+      courseOfGame.push(c);
+      return 'ok';
+   }
+
+   function undoLastMove() {
+      init(whoBegins);
+      courseOfGame.pop();
+   }
+
+   function computeVal(state) {
+      var v = 0;
+      $.each(state.grstate, function (idx, gr) {
+         var n = gr.cnt;
+         v += gr.occupiedBy === STYP.player1 ? n*n*n*n : 0;
+         v -= gr.occupiedBy === STYP.player2 ? n*n*n*n : 0;
+      });
+      return state.whosTurn === STYP.player1 ? v : -v;
+   }
+
+   function miniMax(state, lev, alpha, beta) { // evaluate state recursive, negamax algorithm!
+      state.bestMove = -1;
+
+      if (state.isMill) {
+         return -MAXVAL - lev;
+      }
+      if (lev === 0) {
+         return computeVal(state);
+      }
+      state.maxVal = alpha;
+      for (var c = 0; c < NCOL; c++) {
+         var ordc = ord[c];
+         if (state.hcol[ordc] < NROW) { // try all possible moves
+            var lstate = $.extend(true, {}, state);
+            move(ordc, lstate);
+            var val = -miniMax(lstate, lev - 1, -beta, -state.maxVal);
+            if (val > state.maxVal) {
+               state.maxVal = val;
+               state.bestMove = ordc;
+               if (state.maxVal >= beta) {
+                  return state.maxVal;
+               }
             }
-        });
-        mstate.hcol[c] += 1;
-        mstate.whosTurn = mstate.whosTurn === STYP.player1 ? STYP.player2 : STYP.player1;
-        courseOfGame.push(c);
-        return 'ok';
-    }
+         }
+      }
+      return state.maxVal;
+   }
 
-    function undoLastMove() {
-        init(whoBegins);
-        courseOfGame.pop();
-    }
+   function bestMove() {
+      var lstate = $.extend(true, {}, state);
+      var val = miniMax(lstate, 2, -MAXVAL, +MAXVAL);
+      if (val >= MAXVAL)
+         return lstate.bestMove;
+      lstate = $.extend(true, {}, state);
+      miniMax(lstate, maxLev, -MAXVAL, +MAXVAL);
+      if (lstate.bestMove !== -1)
+         return lstate.bestMove;
+      // there is no best move, just take first possible,
+      var mvs = possibleMoves();
+      return mvs.length ? mvs[0] : -1;
+   }
+   init();
+   ////////////////////////////////////////////////////////////
+   return {
+      isMill: function () {
+         return state.isMill;
+      },
+      setMaxLevel: function (n) {
+         return state.maxLev = n;
+      },
+      getRowOfCol: function (c) {
+         return NROW - state.hcol[c];
+      },
+      whosTurn: function () {
+         return state.whosTurn === STYP.player1 ? 'player1' : 'player2';
+      },
+      getField: function () {
+         return  state.sfeld[c + NCOL * r];
+      },
+      move: move,
+      undoLastMove: undoLastMove,
+      init: init,
+      bestMove: bestMove
 
-    function computeVal(state) {
-        var v = 0;
-        $.each(state.grstate, function (idx, gr) {
-            if (gr.occupiedBy === STYP.player1)
-                v += Math.pow(4, gr.cnt);
-            if (gr.occupiedBy === STYP.player2)
-                v -= Math.pow(4, gr.cnt);
-        });
-        return state.whosTurn === STYP.player2 ? v : -v;
-        ;
-    }
-
-    function miniMax(state, lev, alpha, beta) { // evaluate state recursive, negamax algorithm!
-        state.bestMove = -1;
-
-        if (state.isMill) {
-            return -MAXVAL - lev;
-        }
-        if (lev === 0) {
-            return -computeVal(state);
-        }
-        state.maxVal = alpha;
-        for (var c = 0; c < NCOL; c++) {
-            var ordc = ord[c];
-            if (state.hcol[ordc] < NROW) { // try all possible moves
-                var lstate = $.extend(true, {}, state);
-                move(ordc, lstate);
-                var val = -miniMax(lstate, lev - 1, -beta, -state.maxVal);
-                if (val > state.maxVal) {
-                    state.maxVal = val;
-                    state.bestMove = ordc;
-                    if (state.maxVal >= beta) {
-                        return state.maxVal;
-                    }
-                }
-            }
-        }
-        return state.maxVal;
-    }
-
-    function bestMove() {
-        var lstate = $.extend(true, {}, state);
-        miniMax(lstate, 1, -MAXVAL, +MAXVAL);
-        if (lstate.isMill)
-            return lstate.bestMove;
-        lstate = $.extend(true, {}, state);
-        miniMax(lstate, maxLev, -MAXVAL, +MAXVAL);
-        if (lstate.bestMove !== -1)
-            return lstate.bestMove;
-        // there is no best move, just take first possible,
-        var mvs = possibleMoves();
-        return mvs.length ? mvs[0] : -1;
-    }
-    init();
-    ////////////////////////////////////////////////////////////
-    return {
-        isMill: function () {
-            return state.isMill;
-        },
-        setMaxLevel: function (n) {
-            return state.maxLev = n;
-        },
-        getRowOfCol: function (c) {
-            return NROW - state.hcol[c];
-        },
-        whosTurn: function () {
-            return state.whosTurn === STYP.player1 ? 'player1' : 'player2';
-        },
-        getField: function () {
-            return  state.sfeld[c + NCOL * r];
-        },
-        move: move,
-        undoLastMove: undoLastMove,
-        init: init,
-        bestMove: bestMove
-
-    };
+   };
 }());
 
 QUnit.test('model', function () {
-    function move(m, arr) {
-        $.each(arr, function (i, v) {
-            m.move(v);
-        })
-    }
-    var model = vgmodel;
-    equal(model.setMaxLevel(3), 3, 'SetMaxLevel ok.');
-    equal(model.setMaxLevel(4), 4, 'SetMaxLevel ok.');
-    model.init('player1');
-    equal(model.whosTurn(), 'player1', 'WhosTurn1 ok.');
-    model.init('player2');
-    equal(model.whosTurn(), 'player2', 'WhosTurn2 ok.');
-    model.init('player1');
-    equal(model.whosTurn(), 'player1', 'WhosTurn3 ok.');
-    model.move(4);
-    equal(model.whosTurn(), 'player2', 'WhosTurn4 ok.');
-    model.move(3); // p2
-    equal(model.whosTurn(), 'player1', 'WhosTurn5 ok.');
-    model.move(4);
-    model.move(3);
-    equal(model.whosTurn(), 'player1', 'WhosTurn6 ok.');
-    model.move(4);
-    model.move(3);
-    equal(model.whosTurn(), 'player1', 'WhosTurn7 ok.');
-    //ok(model.bestMove() > 0, 'Evaluate ok');
-    model.init();
-    move(model, [0, 6, 0, 6, 0, 6]);
-    ok(model.bestMove() === 0, 'Evaluate ok');
-    move(model, [1]);
-    ok(model.bestMove() === 6, 'Evaluate ok');
-    model.init();
-    move(model, [3, 3, 4, 4]);
-    ok(model.bestMove() === 5, 'Evaluate ok');
-    model.init();
+   function move(m, arr) {
+      $.each(arr, function (i, v) {
+         m.move(v);
+      })
+   }
+   var model = vgmodel;
+   equal(model.setMaxLevel(3), 3, 'SetMaxLevel ok.');
+   equal(model.setMaxLevel(4), 4, 'SetMaxLevel ok.');
+   model.init('player1');
+   equal(model.whosTurn(), 'player1', 'WhosTurn1 ok.');
+   model.init('player2');
+   equal(model.whosTurn(), 'player2', 'WhosTurn2 ok.');
+   model.init('player1');
+   equal(model.whosTurn(), 'player1', 'WhosTurn3 ok.');
+   model.move(4);
+   equal(model.whosTurn(), 'player2', 'WhosTurn4 ok.');
+   model.move(3); // p2
+   equal(model.whosTurn(), 'player1', 'WhosTurn5 ok.');
+   model.move(4);
+   model.move(3);
+   equal(model.whosTurn(), 'player1', 'WhosTurn6 ok.');
+   model.move(4);
+   model.move(3);
+   equal(model.whosTurn(), 'player1', 'WhosTurn7 ok.');
+   //ok(model.bestMove() > 0, 'Evaluate ok');
+   model.init();
+   move(model, [0, 6, 0, 6, 0, 6]);
+   ok(model.bestMove() === 0, 'Evaluate ok');
+   move(model, [1]);
+   ok(model.bestMove() === 6, 'Evaluate ok');
+   model.init();
+   move(model, [3, 3, 4, 4]);
+   ok(model.bestMove() === 5, 'Evaluate ok');
+   model.init();
 }
 );
 
--- public_html/js/vg/vgmodelstatic.js
+++ public_html/js/vg/vgmodelstatic.js
@@ -1,102 +1,107 @@
 var vgmodelstatic = (function () {
-    "use strict";
-    var DIM = {NCOL: 7, NROW: 7};
-    var STYP = {empty: 0, player1: 1, player2: 2, neutral: 3};
-    var gr = []; // Gewinnreihen
-    var grs = []; // Gewinnreihen pro Feld  
+   "use strict";
+   var DIM = {NCOL: 7, NROW: 7};
+   var STYP = {empty: 0, player1: 1, player2: 2, neutral: 3};
+   var gr = []; // Gewinnreihen
+   var grs = []; // Gewinnreihen pro Feld  
 
-    function getDIM() {
-        return DIM;
-    }
 
-    function berechneGRs(r, c, dr, dc) { // dr = delta row,  dc = delta col
-        var arr = [];
-        while (r >= 0 && r < DIM.NROW && c >= 0 && c < DIM.NCOL) {
-            arr.push(c + DIM.NCOL * r);
-            if (arr.length === 4) {
-                gr.push(arr);
-                return;
+   function berechneGRs(r, c, dr, dc) { // dr = delta row,  dc = delta col
+      function valOfGR(dr, dc) {
+         if (dr === 0) return 1;
+         if (dc === 0) return 8;
+         return 4;
+      }
+      var arr = [];
+      while (r >= 0 && r < DIM.NROW && c >= 0 && c < DIM.NCOL) {
+         arr.push(c + DIM.NCOL * r);
+         if (arr.length === 4) {
+            gr.push({val: valOfGR(dc, dr), arr: arr});
+            return;
+         }
+         c += dc;
+         r += dr;
+      }
+   }
+   function initGRs() {
+      gr = [];
+      for (var r = 0; r < DIM.NROW; r++) {
+         for (var c = 0; c < DIM.NCOL; c++) {
+            berechneGRs(r, c, 0, 1);
+            berechneGRs(r, c, 1, 1);
+            berechneGRs(r, c, 1, 0);
+            berechneGRs(r, c, -1, 1);
+         }
+      }
+      grs = [];
+      for (var i = 0; i < DIM.NCOL * DIM.NROW; i++) {
+         var arr = [];
+         for (var j = 0; j < gr.length; j++) {
+            if (_.contains(gr[j], i)) {
+               arr.push(j);
             }
-            c += dc;
-            r += dr;
-        }
-    }
-    function initGRs() {
-        gr = [];
-        for (var r = 0; r < DIM.NROW; r++) {
-            for (var c = 0; c < DIM.NCOL; c++) {
-                berechneGRs(r, c, 0, 1);
-                berechneGRs(r, c, 1, 1);
-                berechneGRs(r, c, 1, 0);
-                berechneGRs(r, c, -1, 1);
-            }
-        }
-        grs = [];
-        for (var i = 0; i < DIM.NCOL * DIM.NROW; i++) {
-            var arr = [];
-            for (var j = 0; j < gr.length; j++) {
-                if (_.contains(gr[j], i)) {
-                    arr.push(j);
-                }
-            }
-            grs[i] = arr;   
-        }
-        //dump();
-    }
-    function dump() {
-        $.each(gr, function (idx, val) {
-            console.log("gr: " + val);
-        });
-        $.each(grs, function (idx, val) {
-            console.log("grs: " + val);
-        });
-    }
-    function internalTests() {
-        return gr.length === 88
-                && _.isEqual(gr[0], [0, 1, 2, 3])
-                && _.isEqual(gr[1], [0, 8, 16, 24])
-                && _.isEqual(grs[0], [0, 1, 2])
-                && _.isEqual(grs[1], [0, 3, 4, 5])
-                ;
-    }
-    initGRs();
-    var initialState = {
-        hcol: _.range(DIM.NCOL).map(function () {
-            return 0;
-        }), // height of cols
-        grstate: _.range(gr.length).map(function () {
-            return {
-                occupiedBy: STYP.empty,
-                cnt: 0
-            };
-        }),
-        whosTurn: STYP.player1,
-        isMill: 0,
-        cntMoves: 0,
-        bestMove: -1,
-        maxVal: -1
-    };
+         }
+         grs[i] = arr;
+      }
+      //dump();
+   }
+   initGRs();
+   var initialState = {
+      hcol: _.range(DIM.NCOL).map(function () {
+         return 0;
+      }), // height of cols = [0,0,0,...,0];
+      grstate: _.range(gr.length).map(function () {
+         return {
+            occupiedBy: STYP.empty,
+            cnt: 0
+         };
+      }),
+      whosTurn: STYP.player1,
+      isMill: 0,
+      cntMoves: 0,
+      bestMove: -1,
+      maxVal: -1
+   };
+   // debug and test
+   function dump() {
+      $.each(gr, function (idx, val) {
+         console.log("gr: " + val);
+      });
+      $.each(grs, function (idx, val) {
+         console.log("grs: " + val);
+      });
+   }
+   function internalTests() {
+      return gr.length === 88
+         && _.isEqual(gr.arr[0], [0, 1, 2, 3])
+         && _.isEqual(gr.arr[1], [0, 8, 16, 24])
+         && _.isEqual(grs[0].arr, [0, 1, 2])
+         && _.isEqual(grs[1].arr, [0, 3, 4, 5])
+         ;
+   }
 
 // Interface
-    return {
-        // Test + Debug
-        dump: dump,
-        internalTests: internalTests,
-        // Public
-        getDIM: getDIM,
-        getInitialState: function () {
-            return $.extend(true, {}, initialState);
-        },
-        gr: gr,
-        grs: grs,
-        STYP: STYP
-    };
+   return {
+      // Test + Debug
+      dump: dump,
+      internalTests: internalTests,
+      // Public
+      getDIM: function getDIM() {
+         return DIM;
+      },
+      getInitialState: function () {
+         return $.extend(true, {}, initialState);
+      },
+      gr: gr,
+      grs: grs,
+      STYP: STYP
+   };
 }());
 
 QUnit.test('staticModel', function () {
-    //vgmodelstatic.dump();
-    equal(vgmodelstatic.getDIM().NCOL, 7, 'Dimension ok.');
-    equal(vgmodelstatic.getDIM().NROW, 7, 'Dimension ok.');
-    ok(vgmodelstatic.internalTests, "Interne Tests ok");
+   //vgmodelstatic.dump();
+   equal(vgmodelstatic.getDIM().NCOL, 7, 'Dimension ok.');
+   equal(vgmodelstatic.getDIM().NROW, 7, 'Dimension ok.');
+   ok(vgmodelstatic.internalTests, "Interne Tests ok");
 });
    
