0b3eee4ad6f48847a56a047262cf3dc17ed8d004
Erich Brandl <erich.brandl@gmail.com>
2025-03-06 08:45:29 +0100

    use cf-engine.js: first version

--- public_html/js/vg/vg.js
+++
@@ -1,9 +0,0 @@
-const vg = ({
-  init: (id, player) => {
-    vgmodel.init(player)
-    vgview(vgmodel).render(id);
-  },
-  setLevel: vgmodel.setLevel,
-  undoMove: vgmodel.undoMove,
-  getStatistics: vgmodel.getStatistics,
-})
--- public_html/js/vg/vgview.js
+++ public_html/js/vg/vgview.js
@@ -1,66 +1,68 @@
-const vgview = function (m) {
-  const NCOL = vgmodelstatic.DIM.NCOL;
-  const NROW = vgmodelstatic.DIM.NROW;
+const vgview = (cfEngine) => {
+        let thinking = false;
+        let maxThinkingTime = 1000;
+        let moves = []
+        const myAlert = msg => $('<div></div').dialog({
+            title: 'Meldung',
+            buttons: {
+                OK: function () {
+                    $(this).dialog("close");
+                }
+            },
+        }).text(msg).dialog("open");
 
-  const myAlert = msg => {
-    $('<div></div').dialog({
-      buttons: { OK: function () { $(this).dialog("close"); } },
-      autoOpen: false,
-      title: 'Meldung'
-    }).text(msg).dialog("open");
-  }
+        const doMove = (c) => {
+            moves.push(c)
+            cfEngine.doMove(c)
+            const row = cfEngine.DIM.NROW - cfEngine.getHeightOfCol(c);
+            const cls = cfEngine.side() === 1 ? 'red' : 'blue';
+            $($("#vg tr > td:nth-child(" + (c + 1) + ")")[row]).addClass(cls);
+            $("#info").html("Mein letzter Zug:" + (c + 1));
+        }
 
-  const setSpielstein = (c) => {
-    const row = NROW - m.getHeightOfCol(c);
-    const cls = m.whosTurn();
-    $($("#vg tr > td:nth-child(" + (c + 1) + ")")[row]).addClass(cls);
-    $("#info").html("Mein letzter Zug:" + (c + 1));
-  }
+        const actAsAI = () => {
+            if (cfEngine.isMill() || cfEngine.isDraw()) return
+            thinking = true
+            setTimeout(() => {
+                const sc = cfEngine.searchBestMove({maxThinkingTime})
+                thinking = false
+                doMove(sc.bestMoves[0].move);
+                if (cfEngine.isMill()) myAlert("Bedaure, du hast verloren!");
+                else if (cfEngine.isDraw()) myAlert("Gratuliere, du hast ein Remis geschafft!");
+            }, 10)
+        }
 
-  const onClickHandler = c => {
-    return () => {
-      // Player
-      if (m.getHeightOfCol(c) >= NROW) {
-        return;
-      }
-      const res1 = m.move(c);
-      setSpielstein(c);
-      if (res1 === 'isMill') {
-        myAlert("Gratuliere, du hast gewonnen!");
-        return;
-      }
-      if (res1 === 'draw') {
-        myAlert("Gratuliere, du hast ein Remis geschafft!");
-        return;
-      }
+        const onClickHandler = c => {
+            return () => {
+                if (!cfEngine.isAllowedMove(c)) return;
+                doMove(c);
+                if (cfEngine.isMill()) myAlert("Gratuliere, du hast gewonnen!");
+                else if (cfEngine.isDraw()) myAlert("Gratuliere, du hast ein Remis geschafft!");
+                actAsAI()
+            };
+        }
 
-      // Computer
-      const bestMove = m.bestMove();
-      const res2 = m.move(bestMove);
-      setSpielstein(bestMove);
+        const render = divId => {
+            const table = $('<table id="vg"></table>');
+            for (let r = 0; r < cfEngine.DIM.NROW; r++) {
+                const row = $('<tr></tr>');
+                for (let c = 0; c < cfEngine.DIM.NCOL; c++) row.append($('<td></td>').on('click', onClickHandler(c)));
+                table.append(row);
+            }
+            $(divId).empty().append(table);
+            if (cfEngine.side() === cfEngine.Player.blue) actAsAI()
+        }
 
-      if (res2 === 'isMill') {
-        myAlert("Bedaure, du hast verloren!");
-      }
-      if (res2 === 'draw') {
-        myAlert("Gratuliere, du hast ein Remis geschafft!");
-      }
-    };
-  }
+        const undoMove = (beginner) => {
+            cfEngine.init(beginner)
+            moves = moves.slice(0, -2)
+            moves.forEach(m => cfEngine.doMove(m));
+        }
 
-  const render = divid => {
-    const table = $('<table id="vg"></table>');
-    for (let r = 0; r < NROW; r++) {
-      const row = $('<tr></tr>');
-      for (let c = 0; c < NCOL; c++) {
-        row.append($('<td></td>').on('click', onClickHandler(c)));
-      }
-      table.append(row);
+        return { // Interface
+            setMaxThinkingTime: n => maxThinkingTime = n,
+            undoMove,
+            render
+        };
     }
-    $(divid).empty().append(table);
-  }
-
-  return { // Interface
-    render
-  };
-};
+;
