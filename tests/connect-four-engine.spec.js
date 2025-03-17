3f7d1a8f244ad0c27215275db68e49f57144d05c
Erich Brandl <erich.brandl@gmail.com>
2025-03-17 09:09:11 +0100

    connect-four-engine.spec.js:reorganize tests

--- tests/connect-four-engine.spec.js
+++ tests/connect-four-engine.spec.js
@@ -1,6 +1,8 @@
 const cf = require('../js/cf-engine.js');
 
 const range = (n) => [...Array(n).keys()]
+const p = m => m.score === -MAXVAL
+
 
 const {Player, winningRows, winningRowsForFields, MAXVAL} = {...cf}
 
@@ -40,14 +42,15 @@ test('draw - board almost full', () => {
     expect(sc.bestMoves[1].score === 0).toBeTruthy() // -0!!
 })
 
-const handleTest = (t) => {
+const handle = (t) => {
     // if (t.act !== true) return
 
     cf.initGame(t.fen)
     const sc = cf.searchBestMove({maxDepth: t.depth || 42, maxThinkingTime: t.maxThinkingTime || 1000})
 
-    // if (t.act) console.log(t.fen, sc)
-    if (t.depth) expect(sc.depth).toBeGreaterThanOrEqual(t.depth || 1)
+    // if (t.act)
+    // console.log(t.fen, sc)
+    expect(sc.depth).toBe(t.depth )
     if (t.bestMove) {
         const expectedMoves = typeof t.bestMove === "number" ? [t.bestMove] : t.bestMove
         expect(expectedMoves.includes(sc.bestMoves[0].move)).toBeTruthy();
@@ -55,46 +58,37 @@ const handleTest = (t) => {
     if (t.cond) expect(t.cond(sc.bestMoves)).toBeTruthy()
 }
 
-const loosingTests = [
-    {fen: 'blue|030415', depth: 1, cond: bm => bm.every((m) => m.score === -MAXVAL)},
-    {fen: 'blue|33405', depth: 1, cond: bm => bm.every((m) => m.score === -MAXVAL)},
-    {fen: 'blue|0403230012', depth: 8, cond: bm => bm.every((m) => m.score === -MAXVAL)},
-    {fen: 'blue|04032300124332', depth: 4, cond: bm => bm.every((m) => m.score === -MAXVAL)},
-]
+test('eval1', () => handle({fen: 'red|03030', depth: 2, bestMove: 0, cond: bm => bm.slice(1).every(p)}))
+test('eval2', () => handle({fen: 'red|30303', depth: 2, bestMove: 3, cond: bm => bm.slice(1).every(p)}))
+test('eval3', () => handle({fen: 'red|304', depth: 6, bestMove: [2, 5], cond: bm => bm.slice(2).every(p)}))
+test('eval4', () => handle({fen: 'red|30304', depth: 6, bestMove: [2, 5], cond: bm => bm.slice(3).every(p)}))
+test('eval5', () => handle({fen: 'red|264', depth: 6, bestMove: [1, 3, 5], cond: bm => bm.slice(3).every(p)}))
+test('eval6', () => handle({fen: 'red|442', depth: 6, bestMove: [1, 3, 5], cond: bm => bm.slice(3).every(p)}))
+test('eval7', () => handle({fen: 'red|334', depth: 6, bestMove: [2, 5], cond: bm => bm.slice(2).every(p)}))
+test('eval8', () => handle({fen: 'blue|', depth: 8}))
 
-const evalTests = [
-    {fen: 'red|03030', depth: 1, bestMove: 0, cond: bm => bm.slice(1).every((m) => m.score === -MAXVAL)},
-    {fen: 'red|30303', depth: 1, bestMove: 3, cond: bm => bm.slice(1).every((m) => m.score === -MAXVAL)},
-    {fen: 'red|304', depth: 6, bestMove: [2, 5], cond: bm => bm.slice(2).every((m) => m.score === -MAXVAL)},
-    {fen: 'red|30304', depth: 6, bestMove: [2, 5], cond: bm => bm.slice(3).every((m) => m.score === -MAXVAL)},
-    {fen: 'red|264', depth: 6, bestMove: [1, 3, 5], cond: bm => bm.slice(3).every((m) => m.score === -MAXVAL)},
-    {fen: 'red|442', depth: 6, bestMove: [1, 3, 5], cond: bm => bm.slice(3).every((m) => m.score === -MAXVAL)},
-    {fen: 'red|334', depth: 6, bestMove: [2, 5], ond: bm => bm.slice(2).every((m) => m.score === -MAXVAL)},
-    {fen: 'blue|', depth: 8}
-]
+test('loose1', () => handle({fen: 'blue|030415', depth: 2, cond: bm => bm.every(p)}))
+test('loose2', () => handle({fen: 'blue|33405', depth: 2, cond: bm => bm.every(p)}))
+test('loose3', () => handle({fen: 'blue|0403230012', depth: 8, cond: bm => bm.every(p)}))
+test('loose4', () => handle({fen: 'blue|04032300124332', depth: 4, cond: bm => bm.every(p)}))
 
-const winningTests = [
-    {fen: 'red|0606061', depth: 1, bestMove: 6,},
-    {fen: 'red|03043', depth: 2, bestMove: [2, 5]},
-    {fen: 'red|04030023342', depth: 6, bestMove: 6},
-    {fen: 'red|040323001', depth: 6, bestMove: [2, 4, 5]},
-    {fen: 'red|041323233223226361666', depth: 10, bestMove: 5},
-    {fen: 'red|33333535212225510112245514444', depth: 8, bestMove: 4},
-    {fen: 'red|3633241003021332110021266', depth: 12, bestMove: 6},
-    // {fen: 'red|332410233334225', depth: 20, bestMove: 4, maxThinkingTime: 5000},
-    // {fen: 'red|04032300233322434', depth: 22, bestMove: 4, maxThinkingTime: 5000,},
+// winning
+test('win1r', () => handle({fen: 'red|0606061', depth: 1, bestMove: 6,}))
+test('win2r', () => handle({fen: 'red|03043', depth: 2, bestMove: [2, 5]}))
+test('win3r', () => handle({fen: 'red|04030023342', depth: 6, bestMove: 6}))
+test('win4r', () => handle({fen: 'red|040323001', depth: 6, bestMove: [2, 4, 5]}))
+test('win5r', () => handle({fen: 'red|041323233223226361666', depth: 10, bestMove: 5}))
+test('win6r', () => handle({fen: 'red|33333535212225510112245514444', depth: 8, bestMove: 4}))
+test('win7r', () => handle({fen: 'red|3633241003021332110021266', depth: 12, bestMove: 6}))
+// test('win8r', () => handle({fen: 'red|332410233334225', depth: 20, bestMove: 4, maxThinkingTime: 5000}))
+// test('win9r', () => handle({fen: 'red|04032300233322434', depth: 22, bestMove: 4, maxThinkingTime: 5000,}))
 
-    {fen: 'blue|2242', depth: 2, bestMove: 3},
-    {fen: 'blue|5443421244553533332222', depth: 10, bestMove: 4},
-    {fen: 'blue|3135', depth: 12, bestMove: 3},
-    {fen: 'blue|323121', depth: 10, bestMove: 3},
-    {fen: 'blue|4332330222332211', depth: 14, bestMove: 4},
-    {fen: 'blue|333231331011041100', depth: 16, bestMove: 4},
-    {fen: 'blue|323122334104334522', depth: 18, bestMove: 1},
-    // {fen: 'blue|3045', depth: 18, bestMove: 3, maxThinkingTime: 10000},
-
-]
-
-test.each(loosingTests)(`loosing $fen`, handleTest)
-test.each(evalTests)(`eval $fen`, handleTest)
-test.each(winningTests)(`winning $fen $depth`, handleTest)
+test('win1b', () => handle({fen: 'blue|2242', depth: 2, bestMove: 3}))
+test('win2b', () => handle({fen: 'blue|5443421244553533332222', depth: 10, bestMove: 4}))
+test('win2b', () => handle({fen: 'blue|3135', depth: 12, bestMove: 3}))
+test('win3b', () => handle({fen: 'blue|323121', depth: 10, bestMove: 3}))
+test('win4b', () => handle({fen: 'blue|4332330222332211', depth: 14, bestMove: 4}))
+test('win5b', () => handle({fen: 'blue|333231331011041100', depth: 16, bestMove: 4}))
+test('win6b', () => handle({fen: 'blue|323122334104334522', depth: 18, bestMove: 1}))
+// test('win7b', () => handle({fen: 'blue|3036', depth: 12, bestMove: 3}))  //  ~600ms
+// test('win8b', () => handle({fen: 'blue|3045', depth: 18, bestMove: 3, maxThinkingTime: 15000})) // ~6000ms
