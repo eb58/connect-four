ddced7c1f685ac44762a5e8b445504bc4e0ab2bb
eb58 <erich.brandl@gmail.com>
2025-08-05 12:36:03 +0200

    just reformatted

--- run.js
+++ run.js
@@ -7,5 +7,5 @@ cf.init(cf.Player.red)
 moves.forEach((v) => cf.doMove(v))
 const sc = cf.searchBestMove({ maxThinkingTime: 311000 })
 console.log(
-  `DEPTH:${sc.depth} { ${sc.bestMoves.reduce((acc, m) => acc + `${m.move}:${m.score} `, '')}} NODES:${sc.nodes} ${Date.now() - sc.startAt + 'ms'} ${cf.CACHE.info()}} ***`,
+  `DEPTH:${sc.depth} { ${sc.bestMoves.reduce((acc, m) => acc + `${m.move}:${m.score} `, '')}} NODES:${sc.nodes} ${Date.now() - sc.startAt + 'ms'} ${cf.CACHE.info()}} ***`
 )
