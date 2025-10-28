da9aeb6faa4448ed2d28631d7173f21808d76e84
eb58 <erich.brandl@gmail.com>
2025-10-28 20:13:18 +0100

    vg2-engine.js: weitere tests

--- tests/vg2.test.js
+++ tests/vg2.test.js
@@ -3,7 +3,7 @@ const path = require('path')
 const { findBestMove, initGame } = require('../js/vg2-engine')
 
 const readData = (fileName) => {
-  const content = fs.readFileSync(path.join(__dirname, fileName), 'utf-8')
+  const content = fs.readFileSync(path.join(__dirname+"/../data", fileName), 'utf-8')
   return content
     .split('\n')
     .filter((line) => line.trim().length > 0)
@@ -27,12 +27,12 @@ const testData = (fileName, depth= 30) => {
   })
 }
 
-describe('Test_L1_R1', () => testData('Test_L1_R1', 14)) // ~5 sec
+describe('Test_L1_R1', () => testData('Test_L1_R1', 14)) // ~4.5 sec
 describe('Test_L1_R2', () => testData('Test_L1_R2'))
 describe('Test_L1_R3', () => testData('Test_L1_R3'))
 
-describe('Test_L2_R1', () => testData('Test_L2_R1')) // ~ 4.5 sec
+describe('Test_L2_R1', () => testData('Test_L2_R1')) // ~ 1 sec
 describe('Test_L2_R2', () => testData('Test_L2_R2')) // ~ 6 min
 
-describe('Test_L3_R1', () => testData('Test_L3_R1',14)) // 0.5 sec
+describe('Test_L3_R1', () => testData('Test_L3_R1',14)) // ~0.5 sec
 
