acb232869f175d7ddcdbd8aba7b7f0694bd669e6
eb58 <erich.brandl@gmail.com>
2025-10-30 14:13:34 +0100

    cf-winning-rows.test.js: added some tests

--- tests/cf-winning-rows.test.js
+++ tests/cf-winning-rows.test.js
@@ -5,9 +5,19 @@ describe('winning rows ', () => {
   })
   test('winningRowsForFields', () => {
     expect(winningRowsForFields.length).toBe(42)
-    expect(winningRowsForFields[0]).toEqual([0,1,2])
-    expect(winningRowsBS[0].toArray()).toEqual([0,1,2,3])
-    expect(winningRowsBS[1].toArray()).toEqual([0,8,16,24])
-  })
+    expect(winningRowsForFields[0].length).toBe(3)
+    expect(winningRowsForFields[0]).toEqual([0, 1, 2])
+    expect(winningRowsForFields[0].map((i) => winningRowsBS[i].toArray())).toEqual([
+      [0, 1, 2, 3],
+      [0, 8, 16, 24],
+      [0, 7, 14, 21]
+    ])
 
+    expect(winningRowsForFields[6].length).toBe(3)
+    expect(winningRowsForFields[6].map((i) => winningRowsBS[i].toArray())).toEqual([
+      [3, 4, 5, 6],
+      [6, 13, 20, 27],
+      [6, 12, 18, 24]
+    ])
+  })
 })
