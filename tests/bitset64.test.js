a40692ba8ba0bead5b521f35e6434e4352a57e44
eb58 <erich.brandl@gmail.com>
2025-10-30 08:28:37 +0100

    bitset64.js: additional tests

--- tests/bitset64.test.js
+++ tests/bitset64.test.js
@@ -4,18 +4,33 @@ const a = new BitSet64([5, 10])
 const b = new BitSet64([5, 10, 15, 20])
 const c = new BitSet64([1, 2])
 
-test('bitset', () => {
-  expect(a.isSubsetOf(a)).toBeTruthy()
-  expect(a.isSubsetOf(b)).toBeTruthy()
-  expect(b.isSubsetOf(a)).toBeFalsy()
-  expect(c.isSubsetOf(b)).toBeFalsy()
+describe('bitset64', () => {
+  test('isSubsetOf', () => {
+    expect(a.isSubsetOf(a)).toBeTruthy()
+    expect(a.isSubsetOf(b)).toBeTruthy()
+    expect(b.isSubsetOf(a)).toBeFalsy()
+    expect(c.isSubsetOf(b)).toBeFalsy()
+  })
 
-  expect(new BitSet64().count(b)).toBe(0)
-  expect(a.count()).toBe(2)
-  expect(b.count()).toBe(4)
-  expect(c.count()).toBe(2)
+  test('count', () => {
+    expect(new BitSet64().count(b)).toBe(0)
+    expect(a.count()).toBe(2)
+    expect(b.count()).toBe(4)
+    expect(c.count()).toBe(2)
+  })
 
-  expect(c.toBigInt()).toEqual(3n)
-  expect(c.toArray()).toEqual([1, 2])
-  expect(c.toString()).toEqual('[1, 2]')
+  test('toArray ...', () => {
+    expect(c.toBigInt()).toEqual(3n)
+    expect(c.toArray()).toEqual([1, 2])
+    expect(c.toString()).toEqual('[1, 2]')
+  })
+
+  test('clear', () => {
+    const bs = new BitSet64([5, 10, 15, 20])
+    bs.clear(1)
+    expect(bs).toEqual(bs)
+    bs.clear(5)
+    expect(bs.count()).toBe(3)
+  })
+
 })
