ac1f4b3a232c6f482c455ed9cf6759673a5e99be
eb58 <erich.brandl@gmail.com>
2025-10-30 14:13:34 +0100

    cf-winning-rows.test.js: added some tests

--- js/cf-winning-rows.js
+++ js/cf-winning-rows.js
@@ -1,7 +1,18 @@
+const BitSet64 = require('./bitset64')
+
 const range = (n) => [...Array(n).keys()]
 const [COLS, ROWS] = [7, 6]
 
-const BitSet64 = require('./bitset64')
+
+/* layout
+   0  1  2  3  4  5  6
+   7  8  9 10 11 12 13
+  14 15 16 17 18 19 20
+  21 22 23 24 25 26 27
+  28 29 30 31 32 33 34
+  35 36 37 38 39 40 41
+ */
+
 const computeWinningRows = (p, dr, dc) => {
   let { r, c } = p
   const row = []
@@ -19,7 +30,6 @@ const cfWinningRows = range(ROWS)
   .reduce((acc, p) => [...acc, ...computeWinningRows(p, 0, 1), ...computeWinningRows(p, 1, 1), ...computeWinningRows(p, 1, 0), ...computeWinningRows(p, -1, 1)], [])
 
 const winningRowsBS = cfWinningRows.map((x) => new BitSet64(x))
-// console.log(cfWinningRows, winningRowsBS.map(x=>x.toString()))
 const winningRowsForFields = range(COLS * ROWS).map((i) => cfWinningRows.reduce((acc, wr, j) => (wr.includes(i) ? [...acc, j] : acc), []))
-console.log(winningRowsForFields)
-if (typeof module !== 'undefined') module.exports = { winningRowsBS, winningRowsForFields }
+// console.log(winningRowsForFields)
+if (typeof module !== 'undefined') module.exports = { cfWinningRows, winningRowsBS, winningRowsForFields }
