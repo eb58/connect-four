867f44691425e513e8af75057382b40ab9e32372
eb58 <erich.brandl@gmail.com>
2025-10-31 09:03:02 +0100

    bitset64.js: refactor function count

--- js/bitset64.js
+++ js/bitset64.js
@@ -1,5 +1,11 @@
 const range = (n) => [...Array(n).keys()]
 
+const countBits = (n) => {
+  let res = 0
+  for (let i = 0; i < 32 && n ; i++, n = n >> 1) if (!!(n & 1)) res++
+  return res
+}
+
 class BitSet64 {
   constructor(lo, hi) {
     this.lo = 0
@@ -15,10 +21,10 @@ class BitSet64 {
     }
   }
 
-  has = (pos) => !!(pos <= 31 ? this.lo & (1 << pos ) : this.hi & (1 << pos - 32))
-  set = (pos) => (pos <= 31 ? (this.lo |= 1 << pos ) : (this.hi |= 1 << pos - 32))
-  clear = (pos) => (pos <= 31 ? (this.lo &= ~(1 << pos )) : (this.hi &= ~(1 << pos - 32)))
-  toggle = (pos) => (pos <= 31 ? (this.lo ^= 1 << pos ) : (this.hi ^= 1 << pos - 32))
+  has = (pos) => !!(pos <= 31 ? this.lo & (1 << pos) : this.hi & (1 << (pos - 32)))
+  set = (pos) => (pos <= 31 ? (this.lo |= 1 << pos) : (this.hi |= 1 << (pos - 32)))
+  clear = (pos) => (pos <= 31 ? (this.lo &= ~(1 << pos)) : (this.hi &= ~(1 << (pos - 32))))
+  toggle = (pos) => (pos <= 31 ? (this.lo ^= 1 << pos) : (this.hi ^= 1 << (pos - 32)))
 
   intersect = (bs) => new BitSet64(this.lo & bs.lo, this.hi & bs.hi)
   union = (bs) => new BitSet64(this.lo | bs.lo, this.hi | bs.hi)
@@ -27,7 +33,7 @@ class BitSet64 {
 
   equals = (bs) => this.lo === bs.lo && this.hi === bs.hi
   isSubsetOf = (bs) => (this.lo & bs.lo) === this.lo && (this.hi & bs.hi) === this.hi
-  count = () => range(64).reduce((acc, n) => acc + this.has(n), 0)
+  count = () => countBits(this.lo) + countBits(this.hi)
 
   toBigInt = () => (BigInt(this.hi) << 31n) | BigInt(this.lo)
   toArray = () => range(64).reduce((acc, n) => (this.has(n) ? [...acc, n] : acc), [])
