1cc24bdd9182e8471be38d4c8832d00c203efc8c
eb58 <erich.brandl@gmail.com>
2025-11-10 13:25:51 +0100

    cf-engine-2.js: run.js -> depth 30 -> 56.sc

--- js/cf-engine-2.js
+++ js/cf-engine-2.js
@@ -4,25 +4,19 @@ const [COLS, ROWS] = [7, 6]
 const BOARD_SIZE = COLS * ROWS
 const MAXVAL = 1000
 
-// Create a simple PRNG for Zobrist hashing to always get the same result when running tests
-const makePRNG = (seed) => {
-  let state = seed >>> 0
-  return () => {
-    state ^= state << 13
-    state ^= state >>> 17
-    state ^= state << 5
-    return state >>> 0
-  }
-}
-
-const rand = makePRNG(123456789)
-const zobrist = range(BOARD_SIZE).map(() => [0, rand(), rand()])
+const pieceKeys = [
+  227019481, 1754434862, 629481213, 887205851, 529032562, 2067323277, 1070040335, 567190488, 468610655, 1669182959, 236891527, 1211317841, 849223426, 1031915473, 315781957,
+  1594703270, 114113554, 966088184, 2114417493, 340442843, 410051610, 1895709998, 502837645, 2046296443, 1720231708, 1437032187, 80592865, 1757570123, 2063094472, 1123905671,
+  901800952, 1894943568, 732390329, 401463737, 2055893758, 1688751506, 115630249, 391883254, 249795256, 1341740832, 807352454, 2122692086, 851678180, 1154773536, 64453931,
+  311845715, 1173309830, 1855940732, 1662371745, 998042207, 2121332908, 1905657426, 873276463, 1048910740, 1181863470, 136324833, 881754029, 1037297764, 1385633069, 2037058967,
+  398045724, 1522858950, 1892619084, 1364648567, 771375215, 983991136, 260316522, 648466817, 1502780386, 1733680598, 401803338, 2136229086, 718267066, 485772484, 1936892066,
+  1051148609, 1018878751, 1721684837, 1720651398, 2073094346, 526823540, 1170625524, 465996760, 1587572180
+]
 
 const getTTSizeForDepth = (depth) => {
   if (depth >= 38) return (1 << 28) - 1
   if (depth >= 36) return (1 << 26) - 1
   if (depth >= 18) return (1 << 23) - 1
-  if (depth >= 10) return (1 << 18) - 1
   return (1 << 16) - 1
 }
 
@@ -56,7 +50,7 @@ class TranspositionTable {
 
 class Board {
   constructor() {
-    this.bitboards = { 1: [0, 0], 2: [0, 0] } // 64-bit integers simulated with two 32-bit ints, bottom and top bits
+    this.bitboards = [new Uint32Array(2), new Uint32Array(2)] // 64-bit integers simulated with two 32-bit ints, bottom and top bits
     this.currentPlayer = 1
     this.moveHistory = new Uint32Array(COLS * ROWS)
     this.colHeights = new Uint32Array(COLS)
@@ -64,71 +58,38 @@ class Board {
     this.hash = 0
   }
 
-  printBoard = () => {
-    const has = (bb, idx) => (idx < 32 ? bb[0] & (1 << idx) : bb[1] & (1 << (idx - 32)))
-
-    let res = ''
-    for (let r = ROWS - 1; r >= 0; r--) {
-      let row = ''
-      for (let c = 0; c < COLS; c++) {
-        const idx = r * COLS + c
-        if (has(this.bitboards[1], idx)) row += ' X '
-        else if (has(this.bitboards[2], idx)) row += ' O '
-        else row += ' _ '
-      }
-      res += row + '\n'
-    }
-    console.log(res)
-  }
-
   makeMove(col) {
     const row = this.colHeights[col]
-    const index = row * COLS + col
+    const idx = row * COLS + col
 
-    // Bitboards
-    this.bitboards[this.currentPlayer][index < 32 ? 0 : 1] |= 1 << index % 32
-
-    // History & heights
+    this.bitboards[this.currentPlayer][idx < 32 ? 0 : 1] |= 1 << idx % 32
     this.colHeights[col]++
     this.moveHistory[this.moveCount] = col
     this.moveCount++
-
-    // Hash
-    this.hash ^= zobrist[index][this.currentPlayer]
-
-    // Switch player
-    this.currentPlayer = 3 - this.currentPlayer
+    this.hash ^= pieceKeys[this.currentPlayer ? idx : idx + 42]
+    this.currentPlayer = 1 - this.currentPlayer
   }
 
   undoMove(col) {
     col = col || this.moveHistory[this.moveCount - 1]
     const row = this.colHeights[col] - 1
-    const index = row * COLS + col
-
-    // switch player
-    this.currentPlayer = 3 - this.currentPlayer
-
-    // hash
-    this.hash ^= zobrist[index][this.currentPlayer]
-
-    // undo history & heights
+    const idx = row * COLS + col
+    this.currentPlayer = 1 - this.currentPlayer
+    this.hash ^= pieceKeys[this.currentPlayer ? idx : idx + 42]
     this.colHeights[col]--
     this.moveCount--
-
-    // clear bitboards
-    this.bitboards[this.currentPlayer][index < 32 ? 0 : 1] &= ~(1 << index % 32)
+    this.bitboards[this.currentPlayer][idx < 32 ? 0 : 1] &= ~(1 << idx % 32)
   }
 
-   checkWinForBoard = () => {
+  checkWinForBoard = () => {
     const col = this.moveHistory[this.moveCount - 1]
     const row = this.colHeights[col] - 1
-    return this.checkWin(col, row, 3 - this.currentPlayer)
+    return this.checkWin(col, row, 1 - this.currentPlayer)
   }
 
-   checkWinForColumn = (c) => this.checkWin(c, this.colHeights[c], this.currentPlayer)
+  checkWinForColumn = (c) => this.checkWin(c, this.colHeights[c], this.currentPlayer)
 
-   checkWin = (col, row, player) => {
-
+  checkWin = (col, row, player) => {
     const bbLo = this.bitboards[player][0]
     const bbHi = this.bitboards[player][1]
     const has = (idx) => (idx < 32 ? bbLo & (1 << idx) : bbHi & (1 << (idx - 32)))
@@ -156,6 +117,8 @@ class Board {
 }
 
 let tt
+let board = new Board()
+
 const searchInfo = { nodes: 0 }
 
 const negamax = (columns, board, depth, alpha, beta) => {
@@ -177,28 +140,28 @@ const negamax = (columns, board, depth, alpha, beta) => {
     if (board.colHeights[col] < ROWS && board.checkWinForColumn(col)) {
       tt.put(board.hash, 22 - ((board.moveCount + 2) >> 1), depth, flag)
       return { score: 22 - ((board.moveCount + 2) >> 1), move: col }
-  }
+    }
 
   for (const col of columns)
     if (board.colHeights[col] < ROWS) {
-    board.makeMove(col)
-    const child = negamax(columns, board, depth - 1, -beta, -alpha)
-    board.undoMove(col)
+      board.makeMove(col)
+      const child = negamax(columns, board, depth - 1, -beta, -alpha)
+      board.undoMove(col)
 
-    const score = -child.score
+      const score = -child.score
 
-    if (score >= beta) {
-      bestScore = score
-      bestMove = col
-      flag = 2
-      break
+      if (score >= beta) {
+        bestScore = score
+        bestMove = col
+        flag = 2
+        break
+      }
+      if (score > bestScore) {
+        bestScore = score
+        bestMove = col
+      }
+      if (score > alpha) alpha = score
     }
-    if (score > bestScore) {
-      bestScore = score
-      bestMove = col
-    }
-    if (score > alpha) alpha = score
-  }
 
   if (bestScore <= originalAlpha) flag = 3
   else if (bestScore >= beta) flag = 2
@@ -210,23 +173,25 @@ const negamax = (columns, board, depth, alpha, beta) => {
 
 const timeOut = () => Date.now() >= searchInfo.stopAt
 
-const findBestMove = (board, opts) => {
-  opts = { maxDepth: 14, maxThinkingTime: 1000, ...opts }
+const findBestMove = (opts) => {
+  opts = { maxDepth: 20, maxThinkingTime: 1000, ...opts }
   const t = timer()
   searchInfo.nodes = 0
+  searchInfo.stopAt = Date.now() + opts.maxThinkingTime
   let res, depth
   const columns = [3, 2, 4, 1, 5, 0, 6].filter((c) => board.colHeights[c] < ROWS)
   for (depth = 1; depth <= opts.maxDepth; depth++) {
     tt = new TranspositionTable(getTTSizeForDepth(depth))
     res = negamax(columns, board, depth, -MAXVAL, MAXVAL)
+    console.log(`DEPTH:${depth} SCORE: ${res.score} MOVE:${res.move} NODES:${searchInfo.nodes} ${t.elapsedTime()}ms`)
     if (res.score || timeOut()) break
   }
-  // console.log(`DEPTH:${depth} SCORE: ${res.score} MOVE:${res.move} NODES:${nodes} ${t.elapsedTime()}ms`)
+  console.log(`DEPTH:${depth} SCORE: ${res.score} MOVE:${res.move} NODES:${searchInfo.nodes} ${t.elapsedTime()}ms`)
   return { ...res, ...searchInfo, depth, elapsedTime: t.elapsedTime() }
 }
 
 const initGame = (fen) => {
-  const board = new Board()
+  board = new Board()
   const moves = fen
     .trim()
     .split('')
