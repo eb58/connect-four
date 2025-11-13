d86b8a1d2aec09ff3410961d4f6dcf13959ad2f2
eb58 <erich.brandl@gmail.com>
2025-11-13 10:39:10 +0100

    cf-engine.js: checkWinning minor optimization (run.js -> DEPTH:30 SCORE:0 MOVE:3 NODES:178961273 27.519ms)

--- js/cf-board.js
+++ js/cf-board.js
@@ -75,9 +75,11 @@ class Board {
 
     if (row === 0) {
       const bb = this.bitboards[player][0]
-      const bbX = this.bitboards[1 - player][0]
-      if (bb & (1 << 2) && bb & (1 << 3) && !(bbX & 1) && !(bbX & (1 << 4))) return true // _ _ O O _ _ _
-      if (bb & (1 << 3) && bb & (1 << 4) && !(bbX & (1 << 2)) && !(bbX & (1 << 5))) return true // _ _ _ O O _ _
+      if (bb & 8) {
+        const bbX = this.bitboards[1 - player][0]
+        if (bb & 4 && !(bbX & 1) && !(bbX & (1 << 4))) return true // _ _ O O _ _ _
+        if (bb & 16 && !(bbX & (1 << 2)) && !(bbX & (1 << 5))) return true // _ _ _ O O _ _
+      }
     }
     return false
   }
