095fbc69178ea3c5e8563abebec762fd33a37c79
eb58 <erich.brandl@gmail.com>
2025-11-13 11:23:25 +0100

    run.js: use command line parameter

--- run.js
+++ run.js
@@ -1,6 +1,9 @@
-const  Board  = require('./js/cf-board')
-const  findBestMove  = require('./js/cf-engine')
-const fen = ''
-const board = new Board(fen)
-board.print()
-findBestMove(board, { maxThinkingTime: Infinity })
+const Board = require('./js/cf-board')
+const findBestMove = require('./js/cf-engine')
+
+process.argv.slice(2).forEach((fen) => {
+  fen = fen || ''
+  const board = new Board(fen)
+  board.print()
+  findBestMove(board, { maxThinkingTime: Infinity })
+})
